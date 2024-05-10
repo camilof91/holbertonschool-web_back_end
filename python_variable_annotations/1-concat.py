5ae4471f935b1f6dd96bb90fee412b7c32b6efa5
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 16:41:38 -0500

    add funtion concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" concat two str """
+
+
+def concat(a: str, b: str) -> str:
+    """
+    This function concat two strings.
+    Args:
+      a: The first string.
+      b: The second string.
+    Returns:
+      The concat of a and b.
+    """
+    return a + b
