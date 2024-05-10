d545e68c6276226136e4b7bdaa7645ff23b39d36
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 17:26:18 -0500

    define variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ define variables """
 
-if __name__ == "__main__":
-    a: int = 1
-    pi: float = 3.14
-    i_understand_annotations: bool = True
-    school: str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
