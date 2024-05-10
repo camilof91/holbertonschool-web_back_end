75ea89ed1e0203e167f61e839f635687caa0e174
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 16:33:04 -0500

    add funtion add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-''' Sum two float numbers '''
+""" Sum two float numbers """
 
-def add(a: float , b: float)->float:
+
+def add(a: float, b: float) -> float:
     """
     This function adds two floats and returns the sum.
     Args:
