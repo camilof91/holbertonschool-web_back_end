752ad9e056a9a68e618ea3efe542fda7e7b833c3
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 16:47:55 -0500

    add funtion floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" return the floor of a float number """
+import math
+
+def floor(a: float) -> int:
+    """
+    This function return the floor of a float number
+    Args:
+      a: Float number
+    Returns:
+      floor of a
+    """
+    return math.floor(a)
