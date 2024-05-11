9250ace0d2db7847cd79644737a0aa46608bb18f
camilof91 <camilofetecua2011@outlook.com>
2024-05-11 17:59:06 -0500

    add async def wait_n

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-''' '''
+""" """
 from typing import List
 import asyncio
-wait_random = __import__('0-basic_async_syntax').wait_random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     This coroutine spawns n concurrent tasks of the wait_random coroutine,
-    each with the specified max_delay. It then gathers the results (delays) 
+    each with the specified max_delay. It then gathers the results (delays)
     from these tasks and returns them as a list in ascending order.
 
     Args:
         n (int): The number of wait_random coroutines to spawn concurrently.
-        max_delay (float): The upper limit for the random delay in seconds 
+        max_delay (float): The upper limit for the random delay in seconds
                             used by each wait_random coroutine.
 
     Returns:
-        List[float]: A list of delays (float values) in ascending order 
+        List[float]: A list of delays (float values) in ascending order
                     returned by the spawned wait_random coroutines.
     """
     tasks = []
@@ -31,4 +32,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     # No sorting needed due to the nature of asynchronous execution
     return results
-    
