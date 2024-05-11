3a7a712b5e707680db19f71a698d7956e95021c9
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 21:24:04 -0500

    add make_multiplier funtion

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Sum list numbers """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+  """Creates a function that multiplies a number by a given multiplier.
+
+  Args:
+      multiplier (float): The value to use for multiplication.
+
+  Returns:
+      Callable[[float], float]: A new function that takes a float and returns its product with the multiplier.
+  """
+  return lambda x: x * multiplier
