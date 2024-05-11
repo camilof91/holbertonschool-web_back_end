48cfb5c08d5bf5120faf9d9904bd45f842202fa6
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 19:58:14 -0500

    add sto_kv funtion

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def sto_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Calculates the sum of a list of floats.
 
     Args:
