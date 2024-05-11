ca606fceb85cd9281b755912faf2b9116f5ad9b2
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 19:54:18 -0500

    add sto_kv funtion

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Sum list numbers """
+from typing import List, Union
+
+
+def sto_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+    """Calculates the sum of a list of floats.
+
+    Args:
+        input_list: A list of floating-point numbers.
+
+    Returns:
+        float: The sum of all elements in the list.
+    """
+    total = v**2
+
+    return (k, total)
