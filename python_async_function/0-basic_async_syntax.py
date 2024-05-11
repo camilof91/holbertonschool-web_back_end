fe1320cfe686de4773a2a059d4f9c40b8b77fadb
camilof91 <camilofetecua2011@outlook.com>
2024-05-11 17:41:21 -0500

    add async def wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10.0) -> float:
+async def wait_random(max_delay: float = 10) -> float:
     """
     This coroutine waits for a random delay between 0 and the specified
     max_delay (inclusive) seconds and then returns the delay value.
