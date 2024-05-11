4482e873a471cb71d9c7e65540404df4a4790d44
camilof91 <camilofetecua2011@outlook.com>
2024-05-10 19:38:01 -0500

    add sum_list funtion

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -12,7 +12,7 @@ def sum_list(input_list: List[float]) -> float:
     Returns:
         float: The sum of all elements in the list.
     """
-    total:float = 0.0
+    total: float = 0.0
     for num in input_list:
         total += num
     return total
