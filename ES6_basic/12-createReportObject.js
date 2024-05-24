508ae09c65c4a9606bbe977cd6ade430ce37e0ed
camilof91 <camilofetecua2011@outlook.com>
2024-05-24 05:24:01 -0500

    add createReportObject

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
 export default function createReportObject(employeesList) {
+    const allEmployees = { ...employeesList };
+  
     return {
-      allEmployees: {
-        ...employeesList,
-      },
-      getNumberOfDepartments(employeesList) {
-        return Object.keys(employeesList).length;
-      },
+      allEmployees,
+      getNumberOfDepartments: () => Object.keys(allEmployees).length,
     };
   }
