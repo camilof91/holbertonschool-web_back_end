acc76cf807ce509326f009c8bf1b5a60382ee48b
camilof91 <camilofetecua2011@outlook.com>
2024-06-02 20:39:29 -0500

    add compete task

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    resolve('Resolved');
-  });
-}
+    return new Promise((resolve) => {
+      resolve('Resolved');
+    });
+  }
+
